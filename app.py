f35d679d02481cb5890fc11ea486bd6db73ac319
Kevin Harryyanto <kharryyanto@gmail.com>
2020-07-07 19:04:20 +0800

    changed webhook

--- app.py
+++ app.py
@@ -125,11 +125,15 @@ app = Flask(__name__)
 
 
 @app.route('/', methods=['GET'])
+def preset():
+    return "BOT OK", 200
+
+@app.route('/webhook/', methods=['GET'])
 def verify():
     # when the endpoint is registered as a webhook, it must echo back
     # the 'hub.challenge' value it receives in the query arguments
     if request.args.get("hub.mode") == "subscribe" and request.args.get("hub.challenge"):
-        if not request.args.get("hub.verify_token") == 'pdiab': # os.environ["VERIFY_TOKEN"]:
+        if not request.args.get("hub.verify_token") == os.environ["VERIFY_TOKEN"]:
             return "Verification token mismatch", 403
         return request.args["hub.challenge"], 200
 
