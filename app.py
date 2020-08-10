e9b5d7f6b80e329688695548dbeafa83ee282881
kharryyanto <kharryyanto@gmail.com>
2020-08-10 22:55:09 +0800

    Loaded baseline seq2seq model

--- app.py
+++ app.py
@@ -4,123 +4,60 @@ import sys
 import requests
 from flask import Flask, request
 
-import nltk
-nltk.download('punkt')
-from nltk.stem.lancaster import LancasterStemmer
-stemmer = LancasterStemmer()
-
 import numpy as np
-import random
-from tensorflow import keras
-from keras.models import load_model
+import pickle
 
-# Test change to see connection with heroku
+from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing.sequence import pad_sequences
 
-# import our chat-bot intents file
-import json
-with open('intents.json') as json_data:
-    intents = json.load(json_data)
+model = load_model('./chatbot_models/model_big.h5')
+enc_model = load_model('./chatbot_models/encoder_big.h5')
+dec_model = load_model('./chatbot_models/decoder_big.h5')
 
-words = []
-classes = []
-documents = []
-ignore_words = ['?']
-# loop through each sentence in our intents patterns
-for intent in intents['intents']:
-    for pattern in intent['patterns']:
-        # tokenize each word in the sentence
-        w = nltk.word_tokenize(pattern)
-        # add to our words list
-        words.extend(w)
-        # add to documents in our corpus
-        documents.append((w, intent['tag']))
-        # add to our classes list
-        if intent['tag'] not in classes:
-            classes.append(intent['tag'])
+with open('./chatbot_models/tokenizer_big.pickle', 'rb') as handle:
+    tokenizer = pickle.load(handle)
 
-# stem and lower each word and remove duplicates
-words = [stemmer.stem(w.lower()) for w in words if w not in ignore_words]
-words = sorted(list(set(words)))
+maxlen_questions = 223
+maxlen_answers = 132
 
-# remove duplicates
-classes = sorted(list(set(classes)))
+def str_to_tokens(sentence: str):
+    words = sentence.lower().split()
+    tokens_list = list()
+    for current_word in words:
+        result = tokenizer.word_index.get(current_word, '')
+        if result != '':
+            tokens_list.append(result)
+    return pad_sequences([tokens_list],
+                         maxlen=maxlen_questions,
+                         padding='post')
 
-print (len(documents), "documents")
-print (len(classes), "classes", classes)
-print (len(words), "unique stemmed words", words)
+def response(sentence, userID=None, show_details=False):
+    states_values = enc_model.predict(str_to_tokens(sentence))
+    empty_target_seq = np.zeros((1, 1))
+    empty_target_seq[0, 0] = tokenizer.word_index['sos']
+    stop_condition = False
+    decoded_translation = ''
+    while not stop_condition:
+        dec_outputs, h, c = dec_model.predict([empty_target_seq]
+                                              + states_values)
+        sampled_word_index = np.argmax(dec_outputs[0, -1, :])
+        sampled_word = None
+        for word, index in tokenizer.word_index.items():
+            if sampled_word_index == index:
+                if word != 'eos':
+                    decoded_translation += ' {}'.format(word)
+                sampled_word = word
 
-model = load_model('model_ChatBot.h5')
+        if sampled_word == 'eos' \
+                or len(decoded_translation.split()) \
+                > maxlen_answers:
+            stop_condition = True
 
-def clean_up_sentence(sentence):
-    # tokenize the pattern
-    sentence_words = nltk.word_tokenize(sentence)
-    # stem each word
-    sentence_words = [stemmer.stem(word.lower()) for word in sentence_words]
-    return sentence_words
+        empty_target_seq = np.zeros((1, 1))
+        empty_target_seq[0, 0] = sampled_word_index
+        states_values = [h, c]
 
-# return bag of words array: 0 or 1 for each word in the bag that exists in the sentence
-def bow(sentence, words, show_details=False):
-    # tokenize the pattern
-    sentence_words = clean_up_sentence(sentence)
-    # bag of words
-    bag = [0]*len(words)  
-    for s in sentence_words:
-        for i,w in enumerate(words):
-            if w == s: 
-                bag[i] = 1
-                if show_details:
-                    print ("found in bag: %s" % w)
-
-    return(np.array(bag))
-
-# create a data structure to hold user context
-context = {}
-
-ERROR_THRESHOLD = 0.65
-def classify(sentence):
-    # generate probabilities from the model
-    p = bow(sentence, words)
-    
-    d = len(p)
-    f = len(documents)-2
-    a = np.zeros([f, d])
-    tot = np.vstack((p,a))
-    
-    #results = model.predict([bow(sentence, words)])[0]
-    results = model.predict(tot)[0]
-    
-    # filter out predictions below a threshold
-    results = [[i,r] for i,r in enumerate(results) if r>ERROR_THRESHOLD]
-    # sort by strength of probability
-    results.sort(key=lambda x: x[1], reverse=True)
-    return_list = []
-    for r in results:
-        return_list.append((classes[r[0]], r[1]))
-    # return tuple of intent and probability
-    return return_list
-
-def response(sentence, userID, show_details=False):
-    results = classify(sentence)
-    print('Result:',results)
-    # if we have a classification then find the matching intent tag
-    if results:
-        # loop as long as there are matches to process
-        while results:
-            for i in intents['intents']:
-                # find a tag matching the first result
-                if i['tag'] == results[0][0]:
-                    # set context for this intent if necessary
-                    if 'context_set' in i:
-                        if show_details: print ('context:', i['context_set'])
-                        context[userID] = i['context_set']
-
-                    # check if this intent is contextual and applies to this user's conversation
-                    if not 'context_filter' in i or \
-                        (userID in context and 'context_filter' in i and i['context_filter'] == context[userID]):
-                        if show_details: print ('tag:', i['tag'])
-                        # a random response from the intent
-                        return (random.choice(i['responses']))
-            results.pop(0)
+    return(decoded_translation)
 
 history = []
 
